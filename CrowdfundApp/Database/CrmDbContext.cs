0fb627c29e274fc6ee834149b42f6e4db0c0fb4f
Stylianos Xatzikexagias <sxatzikexagias@athtech.gr>
2020-05-15 18:49:27 +0300

    15-5-20

---
+++ CrowdfundApp/Database/CrmDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CrowdfundApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrowdfundApp.Database
+{
+    public class CrmDbContext : DbContext
+    {
+        public DbSet<ProjectCreator> ProjectCreators { get; set; }
+        public DbSet<Backer> Backers { get; set; }
+        public DbSet<Funding> Fundings { get; set; }
+        public DbSet<BackerProject> BackerProjects { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+
+        private readonly string connectionString =
+            "Data Source=localhost;" +
+            "Initial Catalog = CrowdfundApp; " +
+            "Integrated Security = True;";
+
+
+        protected override void OnConfiguring
+            (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+
+
+
+}
--- CrowdfundApp/Models/Project.cs
+++ CrowdfundApp/Models/Project.cs
@@ -9,11 +9,12 @@ namespace CrowdfundApp.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public List<string> Photos { get; set; }
+        public List<string> Images { get; set; }
         public List<string> Videos { get; set; }
-        public List<string> StatusUpdates { get; set; }
+        public string StatusUpdate { get; set; }
         public List<Funding> FundingChoices { get; set; }
         public decimal TotalFundings { get; set; }
+        public decimal Goal { get; set; }
         public bool Active { get; set; }
 
 
