5a768004781435534b74a387e761d5ead9af2cb7
Stylianos Xatzikexagias <sxatzikexagias@athtech.gr>
2020-05-29 13:15:52 +0300

    buttom "addProject"

--- CrowdfundMvc/wwwroot/js/site.js
+++ CrowdfundMvc/wwwroot/js/site.js
@@ -146,3 +146,39 @@ function submitBackerToServer() {
         }
     });
 }
+
+
+
+//function addProject() {
+//    elementid = this.id
+//    projectCreatorId = this.value
+//    //basketId = $("#productsList").value
+//    actionMethod = "POST"
+//    actionUrl = "/apiprojectcreator/addproject"
+//    sendData = {
+//        "projectCreatorId": projectCreatorId,
+//        //basketId": basketId
+//    }
+//    alert(JSON.stringify(sendData))
+//    $.ajax({
+//        url: actionUrl,
+//        dataType: 'json',
+//        type: actionMethod,
+//        data: JSON.stringify(sendData),
+//        contentType: 'application/json',
+//        processData: false,
+//        success: function (data, textStatus, jQxhr) {
+//            //productName = data["name"] 
+//            $('#MyBasket').append('<li><button href="#">' + productName + '</button></li>');
+//            contr = '#' + elementid
+//            $(contr).off('click');
+//            $(contr).click(function () {
+//                alert("cannot buy")
+//            });
+//            //     .attr("disabled", "disabled");
+//        },
+//        error: function (jqXhr, textStatus, errorThrown) {
+//            console.log(errorThrown);
+//        }
+//    });
+//}
